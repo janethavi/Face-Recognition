3040b787c463e4bdfcffcc8d028ad8be656289f3
janethavi <janethavishka@gmail.com>
2019-06-07 14:31:45 +0530

    face values taken

--- FaceChecker.py
+++ FaceChecker.py
@@ -1,16 +1,20 @@
 import numpy as np
 import cv2
 
+face_cascade = cv2.CascadeClassifier('venv/Lib/site-packages/cv2/data/haarcascade_frontalface_alt2.xml')
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x, y, w, h) in faces:
+        print(x, y, w, h)
     # Our operations on the frame come here
     # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
