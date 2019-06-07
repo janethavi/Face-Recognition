e6a2a5ed4f0fa027af6e6ae242ab361cf2c9bc99
janethavi <janethavishka@gmail.com>
2019-06-07 14:54:55 +0530

    rectangle drawn in face

--- FaceChecker.py
+++ FaceChecker.py
@@ -11,6 +11,21 @@ while(True):
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for(x, y, w, h) in faces:
         print(x, y, w, h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        img_item_gray = "my-imagegray.png"
+        img_item_colour = "my-imagecolour.png"
+        cv2.imwrite(img_item_colour, roi_color)
+        cv2.imwrite(img_item_gray, roi_gray)
+
+        color = (255, 0, 0)
+        stroke = 2
+        width = x + w
+        height = y + h
+        cv2.rectangle(frame, (x, y), (width, height), color, stroke)
+
+
+
     # Our operations on the frame come here
     # Display the resulting frame
     cv2.imshow('frame', frame)
