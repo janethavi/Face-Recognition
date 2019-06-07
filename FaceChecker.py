d0a1ef5c0c6a6ef0df13f6515dbf7d702face57b
janethavi <janethavishka@gmail.com>
2019-06-07 18:57:04 +0530

    added a rectangle for the label

--- FaceChecker.py
+++ FaceChecker.py
@@ -26,6 +26,12 @@ while(True):
         id_, conf = recognizer.predict(roi_gray)
         if conf >=45 and conf <=85:
             print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke,cv2.LINE_AA)
+
 
         img_item_gray = "my-imagegray.png"
         img_item_colour = "my-imagecolour.png"
